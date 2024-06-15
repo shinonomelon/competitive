f739aefde03607be58e74dbdbee230d14b44eefe
shinonomelon <70379917+shinonomelon@users.noreply.github.com>
2024-06-15 22:47:26 +0900

    abc358出た

---
+++ ABC/abc358/a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string s, t;
+  cin >> s >> t;
+  if (s == "AtCoder" && t == "Land")
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+
+  return 0;
+}
